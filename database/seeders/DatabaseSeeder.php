0e5180591e1aca15e6e30d4d3088cfe1e13b1cfd
Tharindu Prabash <prabash.tharindu@gmail.com>
2022-12-07 22:24:04 +0530

    details-component

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Category::factory(6)->create();
+        Product::factory(22)->create();
     }
 }
